db61a3e38c38c61860658f0c63902a467b46140a
MatthewMachuca <matthewmachuca457@gmail.com>
2020-11-17 21:33:18 -0800

    fixed insertion sort

--- insertionSort.py
+++ insertionSort.py
@@ -1,12 +1,14 @@
 def insertionSort(arr):
     for i in range(1,len(arr)):
         temp = arr[i]
-        for j in range(i-1,-1,-1):
-            if arr[j] > temp:
-                arr[i] = arr[j]
-                arr[j] = temp
+        j = i-1
+        while (j >= 0) and (arr[j] > temp):
+            arr[j+1] = arr[j]
+            j -= 1
+                
+        arr[j+1]=temp
     return arr
 
 
 
-print(insertionSort([2,5,6,3,9]))
+print(insertionSort([2,2,4,55,34,19,22,5,6,3,9]))
